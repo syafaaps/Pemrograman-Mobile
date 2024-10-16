ab7ef8bb38dfe4bd2371c72c28cc976450cf2ae4
syava aprilia puspitasari <syafaaps3@gmail.com>
2024-10-17 01:48:41 +0700

    Jobsheet 6 Prak 2 Implementasi button row

--- layout_flutter/lib/main.dart
+++ layout_flutter/lib/main.dart
@@ -6,20 +6,23 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-   Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     // Langkah 4: Implementasi title row
     Widget titleSection = Container(
-      padding: const EdgeInsets.all(32), // Padding di sepanjang setiap tepinya 32 piksel
+      padding: const EdgeInsets.all(
+          32), // Padding di sepanjang setiap tepinya 32 piksel
       child: Row(
         children: [
           Expanded(
             /* soal 1 */
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start, // Posisi kolom di awal baris
+              crossAxisAlignment:
+                  CrossAxisAlignment.start, // Posisi kolom di awal baris
               children: [
                 /* soal 2 */
                 Container(
-                  padding: const EdgeInsets.only(bottom: 8), // Padding bawah 8 piksel
+                  padding: const EdgeInsets.only(
+                      bottom: 8), // Padding bawah 8 piksel
                   child: const Text(
                     'Wisata Gunung di Batu',
                     style: TextStyle(
@@ -46,6 +49,40 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+//Prak 2 Langkah 1: Buat method Column _buildButtonColumn
+    Column _buildButtonColumn(Color color, IconData icon, String label) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+  //Prak 2 Langkah 2: Buat widget buttonSection
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Syava Aprilia - 2241760129', // Nama dan NIM Anda
       home: Scaffold(
@@ -55,7 +92,9 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: Column(
             children: [
-              titleSection, // Ganti 'Hello World' dengan titleSection
+              titleSection, // Bagian judul
+              buttonSection, // Bagian tombol
+
             ],
           ),
         ),
@@ -63,4 +102,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
